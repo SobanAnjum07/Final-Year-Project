34f3336d184367ca1bdf09c493aa57e7eac1afd7
Nauman <nauman.samejaa@gmail.com>
2024-12-29 16:20:24 +0500

    added djangongo website with templates

---
+++ chatbot/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+# chatbot/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_active', 'date_joined']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+
+# Register the CustomUser model
+admin.site.register(CustomUser, CustomUserAdmin)
